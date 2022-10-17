c63657cb64e3942203b4f9d26466d67a5d4e90ff
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-17 14:37:31 -0600

    works for the most part, heck it

--- chooseYourCharacter/chooseYourCharacter.js
+++ chooseYourCharacter/chooseYourCharacter.js
@@ -6,7 +6,54 @@ let buttonGo = document.getElementById("buttonGo")
 let buttonRandom = document.getElementById("buttonRandom")
 let characterImg = document.getElementById("characterImg")
 let style = getComputedStyle(characterImg)
+let rowHeight = 51
+let columnWidth = 48
+let cycleBackUpDown = () => {
+	let topNoPx = style.top.replace("px", "")
+	if (topNoPx > 0) {
+		characterImg.style.top = "-153px"
+	} else if (topNoPx < -153) {
+		characterImg.style.top = "0px"
+	}
+	console.log(style.top)
+}
+let cycleBackLeftRight = () => {
+	let leftNoPx = style.left.replace("px", "")
+	if (leftNoPx > 442) {
+		characterImg.style.left = "201.188px"
+	} else if (leftNoPx < 200) {
+		characterImg.style.left = "441.188px"
+	}
+}
 
 buttonUp.addEventListener("click", (e) => {
-	characterImg.style.top
+	let removePx = style.top.replace("px", "")
+	let top = Number(removePx)
+	characterImg.style.top = `${top + rowHeight}px`
+	cycleBackUpDown()
+})
+
+buttonDown.addEventListener("click", (e) => {
+	let removePx = style.top.replace("px", "")
+	let top = Number(removePx)
+	characterImg.style.top = `${top - rowHeight}px`
+	cycleBackUpDown()
+})
+
+buttonLeft.addEventListener("click", (e) => {
+	let removePx = style.left.replace("px", "")
+	let left = Number(removePx)
+	characterImg.style.left = `${left + columnWidth}px`
+	cycleBackLeftRight()
+})
+
+buttonRight.addEventListener("click", (e) => {
+	let removePx = style.left.replace("px", "")
+	let left = Number(removePx)
+	characterImg.style.left = `${left - columnWidth}px`
+	cycleBackLeftRight()
+})
+
+buttonGo.addEventListener("click", (e) => {
+	console.log(style.top)
 })
