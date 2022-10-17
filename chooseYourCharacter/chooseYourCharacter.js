24b159831301cd41e936680347f02b93fcf168bb
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-17 12:41:17 -0600

    working on js, css still wonky

---
+++ chooseYourCharacter/chooseYourCharacter.js
@@ -0,0 +1,12 @@
+let buttonUp = document.getElementById("buttonUp")
+let buttonDown = document.getElementById("buttonDown")
+let buttonLeft = document.getElementById("buttonLeft")
+let buttonRight = document.getElementById("buttonRight")
+let buttonGo = document.getElementById("buttonGo")
+let buttonRandom = document.getElementById("buttonRandom")
+let characterImg = document.getElementById("characterImg")
+let style = getComputedStyle(characterImg)
+
+buttonUp.addEventListener("click", (e) => {
+	characterImg.style.top
+})
